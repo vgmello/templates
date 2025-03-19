87098c00870e128e5d51bd8b0ccd98c37937bed2
Vitor M <4777793+vgmello@users.noreply.github.com>
2025-03-19 18:37:50 +0000

    Bug fix

--- Platform/src/Operations.ServiceDefaults/Messaging/Middlewares/FluentValidationExecutor.cs
+++ Platform/src/Operations.ServiceDefaults/Messaging/Middlewares/FluentValidationExecutor.cs
@@ -69,33 +69,26 @@ public class FluentValidationPolicy : IHandlerPolicy
     }
 }
 
-internal class ValidationResultFrame(Type messageType, MethodCall call) : SyncFrame
+internal class ValidationResultFrame(Type messageType, MethodCall validationMethodCall) : SyncFrame
 {
     private Variable? _context;
 
     public override void GenerateCode(GeneratedMethod method, ISourceWriter writer)
     {
-        static bool IsCommandOrQuery(Type i) => i.IsGenericType &&
-                                                (i.GetGenericTypeDefinition() == typeof(ICommand<>) ||
-                                                 i.GetGenericTypeDefinition() == typeof(IQuery<>));
-
-        static bool HasResultTypeResponse(Type i) => i.GetGenericArguments()[0].IsGenericType &&
-                                                     i.GetGenericArguments()[0].GetGenericTypeDefinition() ==
-                                                     typeof(Result<>);
-
-        var handlerResultResponseType = messageType.GetInterfaces()
-            .FirstOrDefault(i => IsCommandOrQuery(i) && HasResultTypeResponse(i));
+        var handlerResultResponseType = GetQueryOrCommandHandlerResponseType(messageType);
 
         if (handlerResultResponseType is not null)
         {
+            var validationResultVariable = validationMethodCall.Creates.First();
+
             writer.WriteComment("Evaluate whether the execution should stop based on the ValidationFailure list");
-
-            var validationResultVariable = call.Creates.First();
-
             writer.Write($"BLOCK:if ({validationResultVariable.Usage}.Count is not 0)");
 
-            var result = new CastVariable(validationResultVariable, handlerResultResponseType);
+            // Extract Result<T> from the handler response type and cast the validation result to it
+            var resultResponseType = handlerResultResponseType.GetGenericArguments()[0];
+            var result = new CastVariable(validationResultVariable, resultResponseType);
 
+            // Output the validation result as a cascading message
             var cascadingMessage = new CaptureCascadingMessages(result) { Target = _context! };
             cascadingMessage.GenerateCode(method, writer);
 
@@ -111,5 +104,21 @@ internal class ValidationResultFrame(Type messageType, MethodCall call) : SyncFr
         _context = chain.FindVariable(typeof(MessageContext));
 
         yield return _context;
+    }
+
+    /// <summary>
+    ///     Look for handlers that returns an ICommand Or IQuery of ResultOfT
+    /// </summary>
+    private static Type? GetQueryOrCommandHandlerResponseType(Type messageType)
+    {
+        static bool IsCommandOrQuery(Type i) => i.IsGenericType &&
+                                                (i.GetGenericTypeDefinition() == typeof(ICommand<>) ||
+                                                 i.GetGenericTypeDefinition() == typeof(IQuery<>));
+
+        static bool HasResultTypeResponse(Type i) => i.GetGenericArguments()[0].IsGenericType &&
+                                                     i.GetGenericArguments()[0].GetGenericTypeDefinition() ==
+                                                     typeof(Result<>);
+
+        return messageType.GetInterfaces().FirstOrDefault(i => IsCommandOrQuery(i) && HasResultTypeResponse(i));
     }
 }
