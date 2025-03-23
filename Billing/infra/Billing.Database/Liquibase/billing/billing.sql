1796b9d85bb7951c0f49ceae36aa5ff301b37d85
Vitor M <4777793+vgmello@users.noreply.github.com>
2025-03-23 15:02:32 +0000

    Fixing liquibase setup

--- Billing/infra/Billing.Database/Liquibase/billing/billing.sql
+++ Billing/infra/Billing.Database/Liquibase/billing/billing.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset dev_user:"create database" runInTransaction:false
+--changeset dev_user:"create database" runInTransaction:false context:@setup
 CREATE DATABASE billing;
 
 --changeset dev_user:"create billing schema"
