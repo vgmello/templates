f48af65a9b73c2c199793c5f3deed09134e909fc
Vitor M <4777793+vgmello@users.noreply.github.com>
2025-06-26 22:33:22 +0100

    Revert "fix: resolve cashier ID uniqueness issues in database and queries"
    This reverts commit 1f458fc42eb389169fca88af6921b0264f5b5f21.

--- Billing/src/Billing.Api/Cashier/Mappers/CashierMapper.cs
+++ Billing/src/Billing.Api/Cashier/Mappers/CashierMapper.cs
@@ -8,14 +8,5 @@ public static partial class CashierMapper
     [MapperIgnoreSource(nameof(Contracts.Cashier.Models.Cashier.CashierPayments))]
     public static partial Billing.Cashier.Grpc.Models.Cashier ToGrpc(this Billing.Contracts.Cashier.Models.Cashier source);
 
-    public static Billing.Cashier.Grpc.Models.Cashier ToGrpc(this GetCashiersQuery.Result source)
-    {
-        return new Billing.Cashier.Grpc.Models.Cashier
-        {
-            TenantId = string.Empty, // Set empty string instead of null GUID
-            CashierId = source.CashierId.ToString(),
-            Name = source.Name,
-            Email = source.Email
-        };
-    }
+    public static partial Billing.Cashier.Grpc.Models.Cashier ToGrpc(this GetCashiersQuery.Result source);
 }
